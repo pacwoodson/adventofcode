b4c0bb1181649064ec10927024d2c5e4101f02bc
pacwoodson <pac@opac.me>
2025-12-21 14:08:37 +0100

    day 6 pt 1

--- src/day6/mod.rs
+++ src/day6/mod.rs
@@ -1,5 +1,74 @@
+use std::fmt::Display;
+
 use crate::utils;
 
 pub fn solve(input: &String) -> (String, String) {
-    ("0", "0")
+    let lines = utils::lines_to_vec(input);
+
+    // Process input matrix
+    let mut data: Vec<Vec<u64>> = vec![];
+    for (y, line) in lines.iter().enumerate() {
+        if y == lines.len() - 1 {
+            continue;
+        }
+        data.push(vec![]);
+        for row_chunk in line.split(' ') {
+            let parsed = row_chunk.parse::<u64>();
+            if let Ok(n) = parsed {
+                data[y].push(n);
+            }
+        }
+    }
+    // print_matrix(&data);
+
+    // Get operators for each operation array
+    let mut ops: Vec<char> = vec![];
+
+    for row_chunk in lines[lines.len() - 1].split(' ') {
+        if row_chunk == "+" || row_chunk == "*" {
+            ops.push(row_chunk.as_bytes()[0] as char);
+        }
+    }
+
+    let operations_length = data.len();
+    let n_operations = data[0].len();
+
+    // transpose input matrix to get each operation on rows
+    let mut transposed_data = vec![vec![0; operations_length]; n_operations];
+    for (y, row) in data.iter().enumerate() {
+        for (x, col) in row.iter().enumerate() {
+            transposed_data[x][y] = *col;
+        }
+    }
+    // print_matrix(&transposed_data);
+
+    // Do operations on each row (originally columns)
+    let ops_results: Vec<u64> = ops
+        .iter()
+        .enumerate()
+        .map(|(op_index, op)| {
+            transposed_data[op_index]
+                .iter()
+                .copied()
+                .reduce(|acc, n| if *op == '*' { acc * n } else { acc + n })
+                .unwrap_or(0)
+        })
+        .collect();
+
+    // Sum operations
+    let ops_sum: u64 = ops_results.iter().sum();
+
+    (ops_sum.to_string(), "0".to_string())
+}
+
+#[allow(dead_code)]
+fn print_matrix<T: Display>(matrix: &Vec<Vec<T>>) {
+    println!("--");
+    for row in matrix.iter() {
+        for col in row.iter() {
+            print!("{}, ", col);
+        }
+        println!("");
+    }
+    println!("--");
 }
--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,16 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 mod utils;
 
-const SOLVE_FNS: [fn(&String) -> (String, String); 5] = [
+const SOLVE_FNS: [fn(&String) -> (String, String); 6] = [
     day1::solve,
     day2::solve,
     day3::solve,
     day4::solve,
     day5::solve,
+    day6::solve,
 ];
 
 fn main() {
@@ -32,13 +34,15 @@ mod tests {
             let solve_fn = SOLVE_FNS[day];
             let day_input = utils::get_day_input(day + 1);
             let res_test = solve_fn(&day_input.test);
-            let res_input = solve_fn(&day_input.input);
             assert_eq!(res_test.0, day_input.test_result[0], "Part1 error");
             assert_eq!(res_test.1, day_input.test_result[1], "Part2 error");
             print!("[TEST]");
-            if day_input.test_result.len() == 4 {
+            if day_input.test_result.len() > 2 {
+                let res_input = solve_fn(&day_input.input);
                 assert_eq!(res_input.0, day_input.test_result[2], "Real part1 error");
-                assert_eq!(res_input.1, day_input.test_result[3], "Real part2 error");
+                if day_input.test_result.len() == 4 {
+                    assert_eq!(res_input.1, day_input.test_result[3], "Real part2 error");
+                }
                 print!("[REAL]");
             }
             println!("Test for day {} passed.", day + 1);
